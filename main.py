042dd00574c01f862e96067d5d606e614218b8ba
jonathan <97814319+jonathansfx@users.noreply.github.com>
2023-03-09 11:21:57 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# Python Input-Output
+
+
+# Input
+name = input("what is your name?")
+time_of_day = input("What time of day is it?")
+
+
+# Output
+print("hello" + name + ".")
+print("have a great" + time_of_day + "!")
