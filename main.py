a42cf8983625f745fd92a0f737f889e7985843ce
jonathan <97814319+jonathansfx@users.noreply.github.com>
2023-03-14 11:22:31 -0600

    gfghjk

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-# Python Input-Output
+# python menu loop
 
+loop = True
+while loop:
+    # print main manu
+    print("\nMAIN MENU")
+    print("1: option 1")
+    print("2: option 2")
+    print("3: option 3")
+    print("4: EXIT")
 
-# Input
-name = input("what is your name?")
-time_of_day = input("What time of day is it?")
+    # get meny selection from user
+    selection = input("enter selection (1-4): ")
 
-
-# Output
-print("hello" + name + ".")
-print("have a great" + time_of_day + "!")
+    # take action based on menu selection
+    if selection == "1":
+        print("\noption 1")
+    elif selection == "2":
+        print("option 2")
+    elif selection == "3":
+        print("option 3")
+    elif selection == "4":
+        print("EXIT")
+        loop = False
